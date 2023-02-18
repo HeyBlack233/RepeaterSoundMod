f4cebdfcd022f5f35c6735a9aae494209b149969
HeyBlack233 <88659388+HeyBlack233@users.noreply.github.com>
2023-02-18 20:28:15 +0800

    added redstone wire mixin
     added redstone wire mixin and changed repeater mixin behavior

--- src/main/java/heyblack/repeatersound/mixin/RedstoneWireBlockMixin.java
+++ src/main/java/heyblack/repeatersound/mixin/RedstoneWireBlockMixin.java
@@ -4,32 +4,34 @@ import heyblack.repeatersound.RepeaterClickSound;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.block.BlockState;
-import net.minecraft.block.RepeaterBlock;
+import net.minecraft.block.RedstoneWireBlock;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.sound.SoundCategory;
-import net.minecraft.state.property.IntProperty;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(RepeaterBlock.class)
-public class RepeaterBlockMixin
+@Mixin(RedstoneWireBlock.class)
+public abstract class RedstoneWireBlockMixin
 {
-    @Shadow @Final public static IntProperty DELAY;
-
-    @Inject(at = @At("TAIL"), method = "onUse")
-    @Environment(value= EnvType.CLIENT)
-    private void playSound(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit, CallbackInfoReturnable<ActionResult> cir)
+    @Shadow
+    protected static boolean isFullyConnected(BlockState state)
     {
-        float pitch = (0.48f + state.cycle(DELAY).get(DELAY) * 0.02f);
-        world.playSound(player, pos, RepeaterClickSound.BLOCK_REPEATER_CLICK, SoundCategory.BLOCKS, 0.3f, pitch);
+        return false;
+    }
+
+    @Inject(at = @At(value = "RETURN", ordinal = 1), method = "onUse")
+    @Environment(value= EnvType.CLIENT)
+    public void playSound(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit, CallbackInfoReturnable<ActionResult> cir)
+    {
+        float pitch = this.isFullyConnected(state) ? 0.5f : 0.55f;
+        world.playSound(player, pos, RepeaterClickSound.BLOCK_REDSTONE_WIRE_CLICK, SoundCategory.BLOCKS, 0.3f, pitch);
     }
 }
